5d226b2a807970ae39db3b483d696fd6d5284b66
Igor Ferreira <igor.ffp@gmail.com>
2022-11-22 23:00:00 -0300

    novo notebook

--- connection_keys.py
+++ connection_keys.py
@@ -1,7 +1,15 @@
-import pyodbc as pydb
-
+# import pyodbc as pydb
+import os
 from sqlalchemy import create_engine, event
 from urllib.parse import quote_plus
+import json
+
+GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+GCP_SERVICE_ACCOUNT_FILE = os.environ['GCP_SERVICE_ACCOUNT_FILE']
+
+from google.cloud import bigquery
+from google.oauth2 import service_account
+
 #=====================================================
 # Chaves de conexão com Banco de dados
 #------------------------------------------------------ 
@@ -10,17 +18,27 @@ username_bd_corporativo = 'sa'
 password_bd_corporativo = 'P4ssw0rd'
 driver = '{ODBC Driver 17 for SQL Server}'
 
-def connection(database):
-    connection = string_connection_odbc(database)
-    return pydb.connect(connection)
+
+# def connection(database):
+#     connection = string_connection_odbc(database)
+#     return pydb.connect(connection)
+
 
 def alchemy_connection(database):
     con = 'mssql+pyodbc:///?odbc_connect={}'.format(string_connection(database))
     return create_engine(con)
 
+
 def string_connection_odbc(database):
     con = 'DRIVER='+driver+';SERVER='+server_bd_corporativo+';PORT=1433;DATABASE='+database+';UID='+username_bd_corporativo+';PWD='+password_bd_corporativo
     return con
 
+
 def string_connection(database):
     return quote_plus('DRIVER='+driver+';SERVER='+server_bd_corporativo+';DATABASE='+database+';UID='+username_bd_corporativo+';PWD='+password_bd_corporativo)
+
+
+def connect_bq_client():
+    credentials = service_account.Credentials.from_service_account_info(json.loads(GCP_SERVICE_ACCOUNT_FILE))
+    client = bigquery.Client(credentials=credentials, project=GCP_PROJECT_ID)
+    return client
